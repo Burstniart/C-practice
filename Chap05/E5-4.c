0aa784e7026eaa42f623ab4b8d5335c6d38eb08f
Burstniart <v4gr33n@hotmail.com>
2024-07-04 22:07:37 -0600

    Practice Update

--- Chap05/E5-4.c
+++ Chap05/E5-4.c
@@ -7,6 +7,11 @@
 int get_line(char [], int);
 int atoi(char []);
 void reverse (char []);
+void itoa(int, char []);
+int Index(char [], char []);
+int getop(char [], int);
+int getch(void);
+void ungetch(int);
 //VARIABLES
 
 //MAIN
@@ -25,11 +30,23 @@ int main (int argc, char *argv){
   Result = atoi(Number);
   printf("%s - %d\n", Number, Result);
   */
-
+  /*
   printf("%s\n", Hello);
   reverse(Hello);
   printf("%s\n", Hello);
-  
+  */
+  /*
+  int Number = 100;
+  itoa(Number, Text);
+  printf("%s\n", Text);
+  */
+  /*
+  char Find[5] = "d!";
+  int NumIndex = Index(Hello, ", ");
+  printf("Index of !: %d\n", NumIndex);
+  */
+  Result = getop(Text, Lim);
+  printf("Signal: %d - Operand: %s\n", Result, Text);
   return 0;
 }
 
@@ -71,4 +88,86 @@ void reverse (char s[]) /* reverse string s in place */
     *ps++ = *pj;
     *pj-- = c;
   }
+}
+
+
+void itoa(int n, char s[])    /* convert n to characters in s */
+{
+  char *ps = s;
+    int i, sign;
+
+    if ((sign = n) < 0)    /* record sign */
+        n = -n;              /* make n positive */
+    i = 0;
+    do {    /* generate digits in reverse order */
+        *ps++ = n % 10 + '0';     /* get next digit */
+    } while ((n /= 10) > 0); /* delete it */
+    if (sign < 0)
+        *ps++ = '-';
+    *ps = '\0';
+    reverse(s);
+}
+
+int Index(char s[], char t[]) /* return index of t in s, -1 if none */
+{
+  int i, j, k;
+  char *ps = s;
+  char *pt = t;
+  
+  for (i = 0; *ps != '\0'; *ps++, i++) {
+    for (j=i, k=0; *pt!='\0' && *ps==*pt; *ps++, *pt++)
+      ;
+    if (*pt == '\0')
+      return(i);
+  }
+  return (-1) ;
+}
+
+int getop(char s[], int lim) /* get next operator or operand */
+{
+  char *ps = s;
+  int i, c;
+
+  while ((c = getch()) == ' ' || c == '\t' || c == '\n')
+  if (c != '.' && (c < '0' || c > '9'))
+    return(c);
+  *ps++ = c;
+  for (i = 1; (c = getchar()) >= '0' && c <= '9'; i++)
+    if (i < lim)
+      *ps++ = c;
+  if (c == '.') {  /* collect fraction */
+    if (i < lim)
+      *ps++ = c;
+    for (i++; (c=getchar()) >= '0' && c <= '9'; i++)
+      if (i < lim)
+        *ps++ = c;
+  }
+  if (i < lim) {  /* number is ok */
+    ungetch(c);
+    *ps = '\0';
+    return (0);
+  } else { /* it's too big; skip rest of line */
+    while (c != '\n' && c != EOF)
+      c = getchar();
+    s[lim-1] = '\0';
+    return(9);
+  }
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0; /* next free position in buf */
+
+int getch() /* get a (possibly pushed back) character */
+{
+  return((bufp > 0) ? buf[--bufp] : getchar());
+}
+
+void ungetch (int c) /* push character back on input */
+{
+  if (bufp > BUFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    buf[bufp++] = c;
 }
