71a6c9265ad67b1905b7b02fda29f32d9f9dc55c
burstniart <v4gr33n@hotmail.com>
2024-10-10 06:30:48 -0600

    practice update

--- Chap06/E6-7.c
+++ Chap06/E6-7.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define HASHSIZE 100
 
@@ -15,6 +16,10 @@ int hash(char *);
 struct nlist *lookup(char *);
 struct nlist *install(char *, char *);
 
+int main(int argc, char * arg[]) {
+
+  return 1;
+}
 
 int hash(char *s) {
   int hashval;
@@ -33,5 +38,20 @@ struct nlist *lookup(char *s) {
 }
 
 struct nlist *install(char *name, char *def) {
-
+  struct nlist *np, *lookup();
+  int hashval;
+  if ((np = lookup(name)) == NULL) { /*not found*/
+    np = (struct nlist *) malloc(sizeof(*np));
+    if(np == NULL)
+      return(NULL);
+    if((np->def = strdup(name)) == NULL)
+      return(NULL);
+    hashval = hash(np->name);
+    np->next = hashtab[hashval];
+    hashtab[hashval] = np;
+  } else /* already there*/
+    free(np->def); /*free previous definition*/
+  if((np->def = strdup(def)) == NULL)
+    return(NULL);
+  return(np);
 }
