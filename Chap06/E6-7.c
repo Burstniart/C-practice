b7246861101f6a62c40f0a6b52c43e438e206ce0
burstniart <v4gr33n@hotmail.com>
2024-10-11 06:30:48 -0600

    practice update

--- Chap06/E6-7.c
+++ Chap06/E6-7.c
@@ -3,18 +3,33 @@
 #include <stdlib.h>
 
 #define HASHSIZE 100
+#define BUFSIZE 100
+#define ALLOCSIZE 1000 // size of available space
 
+// VARIABLES
+char buf[BUFSIZE];
+int bufp = 0;
 static struct nlist *hashtab[HASHSIZE]; /* pointer table*/
+static char allocbuf[ALLOCSIZE]; /* storage for alloc */
+static char *allocp = allocbuf; /* next free position */
 
+// STRUCTURES
 struct nlist { /*basic table entry*/
   char *name;
   char *def;
   struct nlist *next; /* next entry in chain*/
 };
 
+
+// FUNCTIONS
+int getch(void);
+void ungetch(int);
 int hash(char *);
 struct nlist *lookup(char *);
 struct nlist *install(char *, char *);
+char *alloc(int);
+void freee(char *);
+
 
 int main(int argc, char * arg[]) {
 
@@ -34,14 +49,14 @@ struct nlist *lookup(char *s) {
 
   for (np = hashtab[hash(s)]; np != NULL; np = np->next)
     if (strcmp(s,np->name) == 0)
-      return (np); /*fouund it*/
+      return (np); /*found it*/
 }
 
 struct nlist *install(char *name, char *def) {
   struct nlist *np, *lookup();
   int hashval;
   if ((np = lookup(name)) == NULL) { /*not found*/
-    np = (struct nlist *) malloc(sizeof(*np));
+    np = (struct nlist *) alloc(sizeof(*np));
     if(np == NULL)
       return(NULL);
     if((np->def = strdup(name)) == NULL)
@@ -54,4 +69,46 @@ struct nlist *install(char *name, char *def) {
   if((np->def = strdup(def)) == NULL)
     return(NULL);
   return(np);
+}
+
+struct nlist *uninstall(char *name, char *def) {
+  struct nlist *np, *lookup();
+  int hashval;
+  if ((np = lookup(name)) == NULL) { /*not found*/
+    np = (struct nlist *) alloc(sizeof(*np));
+    if(np == NULL)
+      return(NULL);
+    if((np->def = strdup(name)) == NULL)
+      return(NULL);
+  } else /* already there*/
+    free(np->def); /*free previous definition*/
+  if((np->def = strdup(def)) == NULL)
+    return(NULL);
+  return(np);
+}
+
+//return pointer to n characters
+char *alloc(int n) {
+  if(allocp + n <= allocbuf + ALLOCSIZE) {
+    allocp += n;
+    return(allocp - n); /*old p*/
+  } else /*not enough room*/
+    return (NULL);
+}
+
+// free storage pointed to by p
+void freee(char *p) {
+  if(p >= allocbuf && p < allocbuf + ALLOCSIZE)
+    allocp = p;
+}
+
+int getch(void) {
+  return ((bufp > 0) ? buf[--bufp] : getchar());
+}
+
+void ungetch(int c) {
+  if (bufp > BUFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    buf[bufp++] = c;
 }
